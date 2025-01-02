dd78adfe839f8c6fca6e551c87de296a3449762e
zhijian.qiao <zhijian.qiao@dji.com>
2025-01-02 11:28:51 +0000

    update

--- examples/run.sh
+++ examples/run.sh
@@ -1,9 +1,9 @@
 # run yaml 00-10
 cd ~/LonerSLAM/examples
 # template
-python3 run_loner.py ../cfg/kitti/kitti_00.yaml
-python3 run_loner.py ../cfg/kitti/kitti_01.yaml
+# python3 run_loner.py ../cfg/kitti/kitti_00.yaml
 python3 run_loner.py ../cfg/kitti/kitti_02.yaml
+python3 run_loner.py ../cfg/kitti/kitti_01.yaml
 python3 run_loner.py ../cfg/kitti/kitti_03.yaml
 python3 run_loner.py ../cfg/kitti/kitti_04.yaml
 python3 run_loner.py ../cfg/kitti/kitti_05.yaml
